e1c0ee736cd00ee060a9eb58355b1417f619f2b9
rixmape <rixdonninorecario.mape@bicol-u.edu.ph>
2024-05-18 09:35:05 +0800

    Update chatbot name to Dipcy

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import streamlit as st
 from chatbot import ChatbotAgent
 
 if __name__ == "__main__":
-    st.set_page_config(page_title="Disaster Preparedness Bot", page_icon="🐱‍🚀")
-    st.title("🐱‍🚀 Disaster Preparedness Bot")
+    st.set_page_config(page_title="🤖 Dipcy, your DRR companion.", page_icon="🐱‍🚀")
+    st.title("🤖 Dipcy, your DRR companion.")
 
     st.session_state.setdefault("chatbot", None)
     if not st.session_state.chatbot:
